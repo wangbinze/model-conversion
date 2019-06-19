2dbfde455e25d2309227095220edcf809b1a8f89
binze <40032420+wangbinze@users.noreply.github.com>
2019-06-19 15:24:38 +0800

    1

--- vue-model-conversion/src/views/Info.vue
+++ vue-model-conversion/src/views/Info.vue
@@ -1,12 +1,18 @@
 <template>
 	<div>
 		hello info component
+		<button type="button" @click="add()">添加</button>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "Info"
+		name: "Info",
+		methods: {
+			add() {
+				console.log('add Event form info!!!');
+			}
+		}
 	}
 </script>
 
