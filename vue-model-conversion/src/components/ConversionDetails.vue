6c96ae32c78db0ac8fee930764dc51748f491bad
binze <40032420+wangbinze@users.noreply.github.com>
2019-07-11 17:52:25 +0800

    0

--- vue-model-conversion/src/components/ConversionDetails.vue
+++ vue-model-conversion/src/components/ConversionDetails.vue
@@ -1,22 +1,6 @@
 <template>
 	<div class="conversion">
 		<h3>{{ msg1 }}</h3>
-		<!--<canvas class="demo1">
-			&lt;!&ndash;<input class="upload" type="file" onchange=onUpload2(this.files[0])>&ndash;&gt;
-			&lt;!&ndash;<div id="file-upload-form">
-				<input id="file-upload-input" type="file" name="files[]" multiple="" class="hide">
-				<input type="submit" value="在此处上传或删除文件" id="file-upload-button">
-			</div>&ndash;&gt;
-			模型展示显示位置
-		</canvas>-->
-
-		<!--<div class="section">
-			<a href="javascript:;">
-				<input class="upload" type="file" onchange=onUpload2(this.files[0])>
-				<div class="preview preview2"></div>
-			</a>
-		</div>-->
-
 
 		<!--上传按钮-->
 		<div class="section">
@@ -55,7 +39,7 @@
 
 <style scoped>
 	#demo1 {
-		width: 60%;
+		width: 40%;
 		height: 450px;
 		/*background-color: aquamarine;*/
 		border: 1px solid aquamarine;
