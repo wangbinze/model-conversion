ec3f1745d47030ddc2c3675b50511ada9dbdf963
wangbinze <wangbinze1994@gmail.com>
2019-07-21 23:36:48 +0800

    ConversionDetails.vue

--- vue-model-conversion/src/views/About.vue
+++ vue-model-conversion/src/views/About.vue
@@ -7,7 +7,7 @@
     <br>
     <h2><a href="http://www.paramland.com/" target="_blank">PARAMLAND  |  数地景观</a></h2>
     <br>
-    <!--<p><a href="https://github.com/wangbinze" target="_blank">by Wangbinze</a></p>-->
+    <h3><a href="https://github.com/wangbinze" target="_blank">by Wangbinze</a></h3>
   </div>
 </template>
 
--- vue-model-conversion/src/views/Conversion.vue
+++ vue-model-conversion/src/views/Conversion.vue
@@ -8,7 +8,7 @@
 
 <script>
 
-	import ConversionDetails from '@/components/ConversionDetails.vue'
+	import ConversionDetails from '@/components/ConversionDetails1.vue'
 
 	export default {
 		name: "conversion",
