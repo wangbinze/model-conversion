ae743413b1e066707337f423b4200c491ea77a89
wangbinze <wangbinze1994@gmail.com>
2019-07-24 00:28:17 +0800

    0

--- vue-model-conversion/src/components/ConversionDetails1.vue
+++ vue-model-conversion/src/components/ConversionDetails1.vue
@@ -2,6 +2,19 @@
 	<div>
 		<!--上传按钮-->
 		<!--<input type="file" value=""  id="file"  @change='onUpload'>-->
+		<el-upload
+			class="upload-demo"
+			drag
+			action="https://jsonplaceholder.typicode.com/posts/"
+			multiple
+			:on-change="handleChange"
+			:file-list="fileList">
+			<i class="el-icon-upload"></i>
+			<div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+			<div class="el-upload__tip" slot="tip">只能上传OBJ/OSGB/DAE文件</div>
+		</el-upload>
+
+		<br>
 		<!--模型放置位置-->
 		<div id="container"></div>
 
@@ -20,22 +33,22 @@
 		name: 'ThreeTest',
 		data () {
 			return {
-				camera: null,
-				scene: null,
-				renderer: null,
-				mesh: null
+				fileList:[{
+
+				}]
 			}
 		},
 		methods: {
-
+			handleChange(file, fileList) {
+				this.fileList = fileList.slice(-1);
+			}
 		},
-		mounted(){
-
-		}
 	}
 </script>
 <style scoped>
 	#container {
 		height: 600px;
+		width: 100%;
+		background-color: aquamarine;
 	}
 </style>
